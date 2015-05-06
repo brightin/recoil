6ce0d0dda2b3e6c481e61ed729e689db6127b022
Stefan Roex <info@stefanroex.nl>
2015-05-06 16:41:21 +0200

    Correct module-namespace for Interceptor

--- lib/recoil/interceptor.rb
+++ lib/recoil/interceptor.rb
@@ -1,7 +1,9 @@
-class Interceptor
-  def self.delivering_email(message)
-    if message.to.any? { |email| Email.blacklisted?(email) }
-      message.perform_deliveries = false
+module Recoil
+  class Interceptor
+    def self.delivering_email(message)
+      if message.to.any? { |email| Email.blacklisted?(email) }
+        message.perform_deliveries = false
+      end
     end
   end
 end
