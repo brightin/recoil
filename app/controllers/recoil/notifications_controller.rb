1ce0ea2af76c101684b84ef9b5827b69b4738c99
Stefan Roex <info@stefanroex.nl>
2015-06-30 12:07:57 +0200

    The content_type of a SNS message can be text/plain
    this means we should parse the body as json ourselves.

--- app/controllers/recoil/notifications_controller.rb
+++ app/controllers/recoil/notifications_controller.rb
@@ -1,7 +1,7 @@
 module Recoil
   class NotificationsController < ActionController::Base
     def index
-      Message.for(params).process!
+      Message.for(JSON.parse(request.body)).process!
       head :no_content
     end
   end
