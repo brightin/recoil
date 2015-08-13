dd95c0c095b446715fdd7b5918b26d54b120bc5c
Stefan Roex <info@stefanroex.nl>
2015-08-13 09:32:03 +0200

    Don't perform deliveries when there is no one to send to

--- lib/recoil/interceptor.rb
+++ lib/recoil/interceptor.rb
@@ -1,8 +1,11 @@
 module Recoil
   class Interceptor
     def self.delivering_email(message)
-      # cannot use reject! because .to is not a plain ruby-array.
       message.to = message.to.reject { |email| Email.new(email).blacklisted? }
+
+      if message.to.empty?
+        message.perform_deliveries = false
+      end
     end
   end
 end
