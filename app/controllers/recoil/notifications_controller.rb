f83ead2a525e634dec440fe8db3923f656c26d10
jan-sijmen <jan-sijmen@brightin.nl>
2021-08-06 17:26:34 +0200

    Update controller + spec

--- app/controllers/recoil/notifications_controller.rb
+++ app/controllers/recoil/notifications_controller.rb
@@ -1,7 +1,8 @@
 module Recoil
   class NotificationsController < ActionController::Base
     def index
-      Message.for(JSON.parse(request.raw_post)).process!
+      post = JSON.parse(request.raw_post)
+      Message.for(post).process!
       head :no_content
     end
   end
